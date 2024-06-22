87328dc57df5b22e4888e735ea6dc75d1312e9d4
Sandesh Deshmukh <121145544+Sandesh2704@users.noreply.github.com>
2024-06-22 09:26:59 +0530

    some changes navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,8 +10,8 @@ export default function Navbar() {
   return (
     <>
      <nav className='flex sticky top-0 items-center justify-between py-2 px-3 md:px-16 lg:px-36 bg-gray-100   shadow'>
-      <Link href='/' className='text-2xl md:text-3xl lg:text-4xl font-medium'>EliteShop</Link>
-      <Link href='/cartitem' className='flex items-center gap-2 md:gap-3 bg-yellow-500 py-2 lg:py-3 px-3 lg:px-6 rounded-xl text-base md:text-lg lg:text-xl font-medium'>
+      <Link href='/' className='text-2xl md:text-3xl lg:text-3xl font-medium'>EliteShop</Link>
+      <Link href='/cartitem' className='flex items-center gap-2 md:gap-3 bg-yellow-500 py-2 lg:py-2 px-3 lg:px-6 rounded-xl text-base md:text-lg lg:text-lg font-medium'>
         <MdShoppingCart />
         Cart  <span>{countCart === 0 ? '' : countCart } </span>
       </Link>
